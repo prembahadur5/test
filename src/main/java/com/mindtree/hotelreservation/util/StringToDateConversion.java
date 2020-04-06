c54cec1fcb33d289891842e269091d32b3e0c77b
prembahadur5 <prembahadur5@gmail.com>
2020-04-06 15:18:21 +0530

    commit by prem

---
+++ src/main/java/com/mindtree/hotelreservation/util/StringToDateConversion.java
@@ -0,0 +1,25 @@
+package com.mindtree.hotelreservation.util;
+
+import java.util.Date;
+import java.util.Calendar;
+
+public class StringToDateConversion {
+
+	@SuppressWarnings("deprecation")
+	public static Date getDayMonthYearFromStringDate(String checkInDateString) {
+		
+		String[] stringTokens = checkInDateString.split("-");
+		int[] dateTokens = new int[stringTokens.length];
+		
+		for (int i = 0; i < stringTokens.length; i++) {
+			dateTokens[i] = Integer.parseInt(stringTokens[i]);
+		}
+	
+		Date date = new Date(dateTokens[0], dateTokens[1], dateTokens[2]);
+		
+		
+
+		return date;
+	}
+
+}
